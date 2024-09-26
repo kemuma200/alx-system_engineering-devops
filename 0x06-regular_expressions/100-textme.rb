3b1d50b14bf8b04f2d18b53f92fccf4fa9593c95
monyonko <monyonko.miriam@gmail.com>
2024-09-25 23:06:30 -0700

    Textme

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
 sender = ARGV[0].scan(/from:(.?*)\]/)
-recepient = ARGV[0].scan(/to:(.?*)\]/)
+recipient = ARGV[0].scan(/to:(.?*)\]/)
 flags = ARGV[0].scan(/flags:(.?*)\]/)
 puts [sender, recipient, flags].join(",")
